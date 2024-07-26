4fd46868e67af96a2a07ba7934486e7e0db078c4
Nsikak Akpan <builtbykaksy@gmail.com>
2024-07-26 04:49:10 +0100

    updates to the javascript

--- dist/scripts/cart.js
+++ dist/scripts/cart.js
@@ -2,43 +2,44 @@ import { products } from '../data/products.js'
 
 export let Cart = JSON.parse(localStorage.getItem('carts')) || [];
 let UpdatedProduct = JSON.parse(localStorage.getItem('updatedProducts')) || [];
-let initialProduct;
 
 export const handleCart = () => {
+    displayProductInCart();
+
+    // Adding Items to cart
+    const addToCartBtns = document.querySelectorAll('.best-deals-item-btn');
+    Array.from(addToCartBtns).forEach((btn) => {
+        btn.addEventListener('click', addToCart);
+    });
 
     // Function for adding items to cart
-    function addToCart (){
-        // get all the add to cart buttons
-        const addToCartBtns = document.querySelectorAll('.best-deals-item-btn');
-        console.log(addToCartBtns)
+    function addToCart(e) {
+        const productID = e.target.getAttribute('id');
+        const isProductInCart = Cart.some(cartItem => cartItem.id === productID);
 
-        // Convert the nodelist into an array, and then map through while adding event listeners
-        Array.from(addToCartBtns).forEach((btn, index) => {
-            // console.log(`btn ${index}`)
-            btn.addEventListener('click', (e) => {
-              const productID = e.target.getAttribute('id');
-              console.log(productID)
-              const isProductInCart = Cart.some(cartItems => cartItems.id === productID);
-      
-              if (isProductInCart) {
-                const productInCart = Cart.find(cartItem => cartItem.id === productID);
-                productInCart.quantity++;
-                localStorage.setItem('carts', JSON.stringify(Cart));
-                displayProductInCart();
-              } else {
-                const productToAdd = products.find(product => product.id === productID);
-                productToAdd.quantity = 1;
-                Cart.push(productToAdd);
-                localStorage.setItem('carts', JSON.stringify(Cart));
-                displayProductInCart();
-              }
-            });
-          });
+        if (isProductInCart) {
+            const productInCart = Cart.find(cartItem => cartItem.id === productID);
+            productInCart.quantity++;
+        } else {
+            const productToAdd = products.find(product => product.id === productID);
+            productToAdd.quantity = 1;
+            Cart.push(productToAdd);
+        }
+        localStorage.setItem('carts', JSON.stringify(Cart));
+        displayProductInCart();
     }
 
-    addToCart();
+    // Function for deleting items from the cart
+    function removeProductFromCart(e) {
+        const productID = e.target.getAttribute('data-index');
+        Cart = Cart.filter(item => item.id !== productID);
+        UpdatedProduct = UpdatedProduct.filter(item => item.id !== productID);
+        localStorage.setItem('carts', JSON.stringify(Cart));
+        localStorage.setItem('updatedProducts', JSON.stringify(UpdatedProduct));
+        displayProductInCart();
+    }
 
-    function displayProductInCart(){
+    function displayProductInCart() {
         let cartItemsContainer = document.querySelector('.cart-items-in-cart');
         if (Cart.length === 0) {
             cartItemsContainer.innerHTML = 'No product in cart';
@@ -46,32 +47,189 @@ export const handleCart = () => {
             cartItemsContainer.innerHTML = '';
             Cart.forEach((cartItem) => {
                 cartItemsContainer.innerHTML += `
-                <div class="cart-items-in-cart">
-                    <div class="cart-item-in-cart">
-                        <div class="cart-item-content">
+                <div class="cart-item-in-cart">
+                    <div class="cart-item-content">
                         <div class="cart-item-left">
                             <div class="cart-item-in-cart-img">                                
                                 <img src="${cartItem.image}" alt="">
                             </div>
-                            <p class="cart-item-in-cart-name">
-                            ${cartItem.name}
-                            </p>
+                            <p class="cart-item-in-cart-name">${cartItem.name}</p>
                         </div>
                         <div class="cart-item-right">
                             <div class="cart-item-in-cart-qty">
-                            <button class="cart-inc-btn" id="15b6fc6f-327a-4ec4-896f-486349e85a3d">+</button>
-                            <span>${cartItem.quantity}</span>
-                            <button class="cart-dec-btn" id="15b6fc6f-327a-4ec4-896f-486349e85a3d">-</button>
+                                <button class="cart-inc-btn" id="${cartItem.id}">+</button>
+                                <span>${cartItem.quantity}</span>
+                                <button class="cart-dec-btn" id="${cartItem.id}">-</button>
                             </div>
                             <span class="cart-items-in-cart-price">$${cartItem.priceCents}</span>
                         </div>
-                        </div>
-                        <i class="fa fa-times close-icon-cart" aria-hidden="true" data-index="15b6fc6f-327a-4ec4-896f-486349e85a3d"></i>
                     </div>
+                    <i class="fa fa-times delete-product" aria-hidden="true" data-index="${cartItem.id}"></i>
                 </div>
                 `;
             });
-            
+            // Attach delete event listeners after rendering cart items
+            attachDeleteEventListeners();
         }
     }
+
+    function attachDeleteEventListeners() {
+        const deleteIcons = document.querySelectorAll('.delete-product');
+        deleteIcons.forEach(deleteIcon => {
+            deleteIcon.addEventListener('click', removeProductFromCart);
+        });
+    }
+
+    // Initial call to attach delete event listeners
+    attachDeleteEventListeners();
 }
+
+
+
+
+// import { products } from "../data/products.js";
+
+// export let Cart = JSON.parse(localStorage.getItem('carts')) || [];
+// let UpdatedProduct = JSON.parse(localStorage.getItem('updatedProducts')) || [];
+// let initialProduct;
+// export function handleCart() {
+//   function addToCart() {
+//     const addToCartBtn = document.querySelectorAll('.best-deals-item-btn');
+
+//     Array.from(addToCartBtn).forEach(btn => {
+//       btn.addEventListener('click', (e) => {
+//         const productID = e.target.getAttribute('id');
+//         const isProductInCart = Cart.some(cartItems => cartItems.id === productID);
+
+//         if (isProductInCart) {
+//           const productInCart = Cart.find(cartItem => cartItem.id === productID);
+//           productInCart.quantity++;
+//           localStorage.setItem('carts', JSON.stringify(Cart));
+//           displayProductInCart();
+//         } else {
+//           const productToAdd = products.find(product => product.id === productID);
+//           productToAdd.quantity = 1;
+//           Cart.push(productToAdd);
+//           localStorage.setItem('carts', JSON.stringify(Cart));
+//           displayProductInCart();
+//         }
+//       });
+//     });
+//   }
+//   addToCart();
+
+//   function displayProductInCart() {
+//     let cartItemsContainer = document.querySelector('.cart-items-in-cart');
+//     if (Cart.length === 0) {
+//       cartItemsContainer.innerHTML = 'No product in cart';
+//     } else {
+//       cartItemsContainer.innerHTML = '';
+//       Cart.forEach((cartItem) => {
+//         cartItemsContainer.innerHTML += `
+//         <div class="cart-item-in-cart">
+//           <div class="cart-items-in-cart-img">
+//             <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63e8c4e563db5507951bbfbe_homepad-mini-min.png" alt="">
+//           </div>
+//           <div class="cart-items-in-cart-name">${cartItem.name}</div>
+//           <div class="cart-items-in-cart-qty">
+//             <button class="cart-inc-btn" id="${cartItem.id}">+</button>
+//             <span>${cartItem.quantity}</span>
+//             <button class="cart-dec-btn" id="${cartItem.id}">-</button>
+//           </div>
+//           <div class="cart-items-in-cart-price">$${cartItem.priceCents}</div>
+//           <i class="fa fa-times close-icon-cart" aria-hidden="true" data-index="${cartItem.id}"></i>
+//         </div>
+//         `;
+//       });
+
+//       const incBtn = document.querySelectorAll('.cart-inc-btn');
+//       const decBtn = document.querySelectorAll('.cart-dec-btn');
+
+//       // this is for the incrementing button
+//       Array.from(incBtn).forEach(btn => {
+//         btn.addEventListener('click', (e) => {
+//           const productID = e.target.getAttribute('id');
+//           const productInCart = Cart.find(product => product.id === productID);
+//           let productInStock = UpdatedProduct.find(product => product.id === productID);
+//            initialProduct = products.find(product =>
+//             {
+//               return product.id === productID
+//             })
+//            const initialProductQty = initialProduct.qty;
+//           if (!productInStock) {
+//             productInStock = products.find(product => product.id === productID);
+//             UpdatedProduct.push(productInStock);
+//           }
+//           if (productInStock.qty > 1) {
+//             productInCart.quantity++;
+//             productInStock.qty--;
+//             localStorage.setItem('carts', JSON.stringify(Cart));
+//             localStorage.setItem('updatedProducts', JSON.stringify(UpdatedProduct));
+//             displayProductInCart();
+//           } else {
+//             alert('We are out of stock');
+//           }
+//         });
+//       });
+
+//       // this is for the decrementing button
+//       Array.from(decBtn).forEach(btn => {
+//         btn.addEventListener('click', (e) => {
+//           const productID = e.target.getAttribute('id');
+//           const productInCart = Cart.find(product => product.id === productID);
+          
+//           let productInStock = UpdatedProduct.find(product => product.id === productID);
+
+//           if (!productInStock) {
+//             productInStock = products.find(product => product.id === productID);
+//             UpdatedProduct.push(productInStock);
+//           }
+
+//           if (productInCart.quantity > 1) {
+//             productInCart.quantity--;
+//             productInStock.qty++;
+//             localStorage.setItem('carts', JSON.stringify(Cart));
+//             localStorage.setItem('updatedProducts', JSON.stringify(UpdatedProduct));
+//             displayProductInCart();
+//           } else {
+//             alert('Quantity cannot be less than 1');
+//           }
+//         });
+//       });
+//     }
+
+//     function removeProductFromCart()
+//     {
+//       const deleteIcons = document.querySelectorAll('.close-icon-cart');
+//       Array.from(deleteIcons).forEach(deleteIcon =>
+//         {
+//           deleteIcon.addEventListener('click',(e)=>
+//           {
+//             const productID = e.target.getAttribute('data-index');
+
+           
+//               const newArray = Cart.filter( item =>
+//                 {
+//                   return item.id !== productID
+//                 })
+//                 const newArray2 = UpdatedProduct.filter( item =>
+//                   {
+//                     return item.id !== productID
+//                   })
+//                 Cart = newArray
+//                 UpdatedProduct = newArray2
+//                 localStorage.setItem('carts', JSON.stringify(Cart));
+//                 localStorage.setItem('updatedProducts', JSON.stringify(UpdatedProduct));
+//                 displayProductInCart();
+
+//           })
+//         })
+//     }
+//     removeProductFromCart()
+//   }
+//   displayProductInCart();
+
+  
+//   // 
+// }
+
--- dist/scripts/navigation.js
+++ dist/scripts/navigation.js
@@ -85,3 +85,157 @@ export default function handleNavigation () {
     }
     displayProductCategories()
 }
+
+
+
+
+
+
+// import { products } from "../data/products.js";
+// export default function handleNavigation()
+// {
+//     const engTextHolder = document.querySelector('.navigation-section-greenbar-eng');
+//     const langList = document.querySelector('.navigation-section-greenbar-eng-items');
+//     engTextHolder.addEventListener('click', showLanguage)
+//     function showLanguage()
+//     {
+//         langList.classList.toggle('display-none')
+//     }
+//     const categoriesHolder = document.querySelector('.navigation-section-categories-text');
+//     const catList = document.querySelector('.navigation-section-categories-holder');
+//     categoriesHolder.addEventListener('click', showCategories);
+
+//     function showCategories()
+//     {
+//         catList.classList.toggle('display-none')
+//     }
+//     const locationsHolder = document.querySelector('.navigation-section-greenbar-locations');
+//     const diffLocations = document.querySelector('.navigation-section-greenbar-diff-locations');
+//     locationsHolder.addEventListener('click', showLanguages);
+
+//     function showLanguages()
+//     {
+//         diffLocations.classList.toggle('display-none')
+//     }
+    
+//     // display products categories
+//     function displayProductCategories()
+//     {
+//         let categoriesHolder = document.querySelector('.navigation-section-categories-item-holder');
+//         const categoryCount = {}
+       
+//         products.forEach((product)=>
+//             {
+             
+//                 product.category.forEach((categories)=>{
+//                     if(categoryCount[categories])
+//                         {
+//                             categoryCount[categories]++
+//                         }else
+//                         {
+//                             categoryCount[categories] = 1
+//                         }             
+//                 })
+
+//             })
+            
+          
+//             categoriesHolder.innerHTML = '';
+//             for(const categoryKeys in categoryCount)
+//                 {
+//                    console.log(categoryKeys)
+//                     categoriesHolder.innerHTML += 
+//                     `
+//                     <div class="navigation-section-categories-item flex flex-align">
+//                     <div><img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" alt="image loading"></div>
+//                     <div class="flex flex-column nav-categories-name-qty">
+//                       <span>${categoryKeys}</span>
+//                       <span>${categoryCount[categoryKeys]} items</span>
+//                     </div>
+//                   </div>
+//                     `;
+
+
+//                 }
+
+
+
+//     }
+//     displayProductCategories()
+
+//     // arrow function syntax
+//     const displaySearchItems = ()=>
+//     {
+//       const searchedValueHolder = document.querySelector('.navigation-section-searchbar');
+      
+//       // the keyup event listener is triggered when ever any key is been clicked or pressed
+//       searchedValueHolder.addEventListener('keyup', (e)=>
+//       {
+//         const searchedValue = searchedValueHolder.value;
+//         console.log(e)
+//         // e.key == "Enter" simply means that this function would only be fired if the user clicks on Enter
+//         if(e.key == "Enter")
+//         {
+//           // This array is used to store the products that matched our condition in our IF-Else statement in Line 88
+//           let searchedProductArray = []
+//           products.forEach(product => 
+//             {
+//               //  the essense of this code is to check if what the user searched on matches any of the names in our Products. The reason why We used toLowerCase is because JS is case sensitive to we want everything (product.name) and (searchedValue) to be on the same case which is lowe case
+//               if(product.name.toLowerCase().includes(searchedValue.toLowerCase()))
+//               {
+//                 // we need to select the search box holder div so we could remove the display-none class we added to it. This would make it visible. Use ctrl + f to see where this div is on our index.html file; 
+//                 document.querySelector('.navigation-section-search-box').classList.remove('display-none');
+
+//                 // After the condition has been met, we would push the product or products that would meet the condition
+//                 searchedProductArray.push(product)
+
+//                 // Select the div that will be updated when we search the product 
+
+//                  let searchedProductHTML = document.querySelector('.navigation-section-search-items-holder');
+//                  searchedProductHTML.innerHTML = '';
+
+//                 //  Loop through the searchedProducts array 
+//                  searchedProductArray.forEach((searchedProduct)=>
+//                  {
+//                   // dynamically display the searched products here 
+//                   searchedProductHTML.innerHTML += `
+//                   <div class="navigation-section-search-items-holder flex">
+//                   <div class=" flex flex-column flex-justify-center">
+//                     <h5>${searchedProduct.name}</h5>
+//                     <span>$${searchedProduct.priceCents}</span>
+//                   </div>
+//                 </div>
+//                   `
+//                  })
+
+              
+//               }
+//             })
+//         }else
+//         {
+          
+//           return
+//         }
+//       })
+      
+      
+     
+
+//     }
+//     displaySearchItems()
+    
+//     const displayMobileCategories = ()=>
+//     {
+//       const categoriesDiv = document.querySelector('.navigation-whitebar-categories');
+//       const hambugerMenu =  document.querySelector('.navigation-section-hambugerMenu');
+//       hambugerMenu.addEventListener('click', ()=>
+//       {
+//          categoriesDiv.classList.toggle('flex');
+//         console.log(categoriesDiv)
+//       })
+//     }
+//     displayMobileCategories()
+
+
+ 
+// }
